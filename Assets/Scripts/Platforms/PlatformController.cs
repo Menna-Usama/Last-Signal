1bc0c2ea23a526db7bd08f1b14466c4d3f455039
Menna Usama <20240615@stud.fci-cu.edu.eg>
2026-08-07 14:41:46 +0300

    Falling Platforms

--- Assets/Scripts/Platforms/PlatformController.cs
+++ Assets/Scripts/Platforms/PlatformController.cs
@@ -1,4 +1,5 @@
 using UnityEngine;
+using UnityEngine.Rendering;
 
 public class PlatformController : MonoBehaviour
 {
@@ -40,6 +41,9 @@ public class PlatformController : MonoBehaviour
 
             case PlatformType.Moving:
                 return PlatformType.Blinking;
+
+            case PlatformType.Blinking:
+                return PlatformType.Falling;
 
             default:
                 return PlatformType.Normal;
--- Assets/Scripts/Platforms/PlatformTypes.cs
+++ Assets/Scripts/Platforms/PlatformTypes.cs
@@ -2,5 +2,6 @@ public enum PlatformType
 {
     Normal,
     Moving,
-    Blinking
+    Blinking,
+    Falling
 }
