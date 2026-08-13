e228cd472a34a5562eb4e1f77ce4b27bb14b6e66
Reng-cmy <tahinistain@gmail.com>
2026-08-13 14:15:00 +0300

    Logs put into segment three

---
+++ Assets/Scripts/Platforms/FallingPlatformCopy.cs
@@ -0,0 +1,62 @@
+using UnityEngine;
+
+public class FallingPlatformCopy : MonoBehaviour
+{
+    [SerializeField] private Vector3 fallOffset = new Vector3(0f,-2f, 0f);
+    public float moveSpeed = 2f;
+
+
+    private Vector3 _topPos;
+    private Vector3 _bottomPos;
+    private Vector3 _nextPosition;
+    private Vector3 _lastPosition;
+
+    private Transform _player;
+
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        _topPos = transform.position;
+        _bottomPos = transform.position + fallOffset;
+        _nextPosition = _topPos;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = Vector3.MoveTowards(transform.position, _nextPosition, moveSpeed * Time.deltaTime);
+    }
+
+    private void LateUpdate()
+    {
+
+        if (_player != null)
+        {
+            Vector3 delta = transform.position - _lastPosition;
+            _player.position += delta;
+        }
+
+        _lastPosition = transform.position;
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            _player = collision.transform;
+            _nextPosition = _bottomPos;
+        }
+    }
+
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            _player = null;
+            _nextPosition = _topPos;
+        }
+    }
+}
+
+
